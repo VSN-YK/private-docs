690c5f0ffeb8f460241744f2a706cea4706477f3
Yutetsu Kuji <=vsn_2293806@i.softbank.jp>
2020-01-28 11:53:25 +0900

    Add go Practice file

---
+++ go/testpackage/packageA/packageA.go
@@ -0,0 +1,8 @@
+/******************Testing Package A************************/
+
+package packageA
+
+// DispPackageA : Displaying "PackageA"
+func DispPackageA () string{
+  return "From PackageA"
+}
