5794b64751ac89ffd5533b87784abd0cdd1a09e3
Y.K <yk@gmail.com>
2020-05-18 08:05:22 +0900

    [Modify] Correspond with LinuxOS

--- docker-resource/source/shell/deploy-private-registry.sh
+++ docker-resource/source/shell/deploy-private-registry.sh
@@ -3,11 +3,15 @@
 set -eux
 
 readonly DOCKER_MOUNT_DIR="docker_registry"
+readonly OS_SIGNATURE_LINUX="LINUX"
+readonly OS_SIGNATURE_MAC="Darwin"
 
+# @param custom_port is a optinal value. : 5000
 function build_private_registry() {
+  local custom_port="${1:-5000}"↲
   docker run \
     -d \
-    -p 5000:5000 \
+    -p ${custom_port}:5000 \
     -v ${DOCKER_HOME}/${DOCKER_MOUNT_DIR}:/var/lib/registry \
     -h docker_registry \
     registry:latest
@@ -25,12 +29,17 @@ function build_docker_frontend(){
 }
 
 DOCKER_HOME=$PWD
-local_ip=$(ifconfig en0 | awk '/inet / {print $2}')
+local_ip=""
 
 if [[ ! -d ${DOCKER_HOME}/${DOCKER_MOUNT_DIR} ]]; then
   mkdir ${DOCKER_MOUNT_DIR}
 fi
 
+my_os=$(uname)
+case "$my_os" in 
+  "${OS_SIGNATURE_MAC}" )  local_ip=$(ifconfig en0  | awk '/inet / {print $2}') ;;↲
+  "${OS_SIGNATURE_LINUX}") local_ip=$(ifconfig eth0 | awk '/inet / {print $2}')↲;;
+esac
 # exec function
 build_private_registry
 build_docker_frontend ${local_ip}
